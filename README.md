bcc81044d624a7b6770b0c8b944efeb917301684
Sujit Pranav <ssolipuram@denkensolutions.com>
2019-10-01 07:00:59 +0530

    updated Read me file, seed data

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # spiceworks
 It's a sample repo for nested comments
+
+
+1) Clone the repository
+
+
+git@github.com:iamsujitpranav/spiceworks.git
+
+
+2) Make sure you have following versions
+
+ruby 2.6.3, rails 6.0, postgres 9.5 or above 
+
+3) Bundle the repo
+
+bundle install
+
+4) Create the database
+
+rake db:create
+
+5) Run migrations
+
+rake db:migrate
+
+6) Run the seed data
+
+rake db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(first_name: "Some", last_name: "user", email: "someuser@gmail.com", password: "123456", gender: "male")
+post = user.posts.create(title: "Title1", body: "Content for post")
+comment = post.comments.create(body: "this is a comment", user: user)
+nested_comment = comment.comments.create(body: "this is nested comment", user: user)
+nested_comment2 = comment.comments.create(body: "this is nested comment2", user: user)
+nested_comment3 = nested_comment.comments.create(body: "this is nested comment3", user: user)
+nested_comment4 = nested_comment3.comments.create(body: "this is nested comment4", user: user)
+nested_comment5 = nested_comment2.comments.create(body: "this is nested comment5", user: user)
+user2 = User.create(first_name: "Another", last_name: "user", email: "anotheruser@gmail.com", password: "123456", gender: "male")
+another_user_comment = post.comments.create(body: "this is a comment", user: user2)
+another_user_nested_comment = comment.comments.create(body: "this is nested comment", user: user2)
